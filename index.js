ff9911d596512201a37cf9196c97202e66bfc60f
egoheru <egoherokugit@gmail.com>
2020-08-30 05:09:37 -0700

    changed

---
+++ index.js
@@ -0,0 +1,39 @@
+require('./db')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require ('cors')
+
+const path = require ('path')
+
+// importing files
+var postMessageRoutes = require('./controllers/postMessageController')
+
+// Define Global Variables
+var app = express()
+const log = console.log;
+const PORT = process.env.PORT || 4000; // Step 1
+
+
+// //Dummy test
+// app.get('/', () => {
+//     res.json({
+//         message: 'hello world'
+//     })
+// })
+
+
+//Configuration of Variables 
+app.use(bodyParser.json())
+app.use(cors({origin:'http://localhost:3000'}))
+app.listen(PORT,()=>console.log('Server started at : ' + PORT) )
+
+//Dummy test
+app.get('/', (req, res) => {
+    res.json({
+        message: 'hello world'
+    })
+})
+
+// app.use('/',postMessageRoutes)
+app.use('/postMessages',postMessageRoutes)
+
